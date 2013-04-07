de5e5e050d3bef8ce20c70a31443964ddc6eb57f
Mihail Kornilov <mihan_k@mail.ru>
2013-04-07 18:57:17 +0400

    test

--- nopage_tpl.php
+++ nopage_tpl.php
@@ -12,7 +12,7 @@ $(document).ready(function(){
 
 <DIV class=nopage>
 	Ошибка: несуществующая страница.<BR><BR>
-	<DIV class=vkButton onclick=location.href='<?php echo $URL."&my_page=".$_GET['parent']; ?>';><BUTTON>Назад</BUTTON></DIV>
+	<DIV class=vkButton onclick="location.href='<?=$URL."&my_page=".$_GET['parent']; ?>';"><BUTTON>Назад</BUTTON></DIV>
 </DIV>
 
 
